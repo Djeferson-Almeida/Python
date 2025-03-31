df7f2133da28d25687365b4448bc63c7607531ad
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-03-31 13:20:06 -0300

    First Example - ELIF

---
+++ IF_Teste/main.py
@@ -0,0 +1,10 @@
+idade = 45
+
+if idade < 15:
+  print('Criança')
+elif(idade>= 15 and idade <= 19):
+  print('Adolescente')
+elif(idade>=19 and idade<60):
+  print('Adulto')
+else:
+  print('Idoso')
