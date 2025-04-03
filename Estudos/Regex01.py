c53c9919a0d5471c2c0d21537411f1f9672a47d4
Djeferson Almeida <djefersonalmeida20@gmail.com>
2025-04-03 17:23:44 -0300

    Anotations and Regex implements

--- Estudos/Regex01.py
+++ Estudos/Regex01.py
@@ -1,7 +1,14 @@
 import re
 
-phone_number = re.compile(r'\(\d{2}\) \d{5}-\d{4}')
+phone_number = re.compile(r'(\(\d{2}\)) (\d{5}-\d{4})')
 result = phone_number.search('My number is (48) 99658-4456, please call me after 7pm')
 
-print('Phone number encountered: ' + result.group())
-                          
+print('DDD: ' + result.group(1))
+print('Phone number:' + result.group(2))
+
+
+#Outras maneiras de chamar os grupos   
+ddd,phone = result.groups()
+
+print(ddd)
+print(phone)
